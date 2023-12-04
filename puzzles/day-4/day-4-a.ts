35da59ed96a2a7345c60925d04ade28fcadcc2cf
Roy Kakkenberg <me@roykakkenberg.nl>
2023-12-04 07:28:16 +0100

    Add day 4 solutions

--- puzzles/day-4/day-4-a.ts
+++ puzzles/day-4/day-4-a.ts
@@ -1,9 +1,31 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+interface LotteryNumbers {
+  winning: number[];
+  mine: number[];
+}
+
 export async function day4a(dataPath?: string) {
-  const data = await readData(dataPath);
-  return 0;
+  const data = (await readData(dataPath)).filter((d) => d !== '');
+
+  const lotteryNumbers: LotteryNumbers[] = data.map((line) => {
+    const [id, numbers] = line.split(': ');
+    const [winningStrings, myStrings] = numbers.split(' | ');
+    return {
+      winning: winningStrings.split(' ').map((n) => parseInt(n.trim(), 10)).filter((i) => !Number.isNaN(i)),
+      mine: myStrings.split(' ').map((n) => parseInt(n.trim(), 10)).filter((i) => !Number.isNaN(i)),
+    }
+  });
+
+  const winningNumbers = lotteryNumbers.map((l) =>
+    l.mine.filter((m) =>l.winning.includes(m)));
+  const points = winningNumbers.map((l) => {
+    if (l.length === 0) return 0;
+    return 2 ** (l.length - 1);
+  })
+
+  return points.reduce((total, p) => total + p, 0);
 }
 
 const answer = await day4a();
--- puzzles/day-4/day-4-b.ts
+++ puzzles/day-4/day-4-b.ts
@@ -1,9 +1,35 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+interface LotteryNumbers {
+  winning: number[];
+  mine: number[];
+}
+
 export async function day4b(dataPath?: string) {
-  const data = await readData(dataPath);
-  return 0;
+  const data = (await readData(dataPath)).filter((d) => d !== '');
+
+  const lotteryNumbers: LotteryNumbers[] = data.map((line) => {
+    const [id, numbers] = line.split(': ');
+    const [winningStrings, myStrings] = numbers.split(' | ');
+    return {
+      winning: winningStrings.split(' ').map((n) => parseInt(n.trim(), 10)).filter((i) => !Number.isNaN(i)),
+      mine: myStrings.split(' ').map((n) => parseInt(n.trim(), 10)).filter((i) => !Number.isNaN(i)),
+    }
+  });
+
+  const winningNumbers = lotteryNumbers.map((l) =>
+    l.mine.filter((m) =>l.winning.includes(m)));
+
+  const instances: number[] = new Array(winningNumbers.length).fill(1);
+  winningNumbers.forEach((winnings, index) => {
+    const copies = instances[index];
+    for (let i = 1; i <= winnings.length; i++) {
+      instances[index + i] = instances[index + i] + copies;
+    }
+  })
+
+  return instances.reduce((total, p) => total + p, 0);
 }
 
 const answer = await day4b();
