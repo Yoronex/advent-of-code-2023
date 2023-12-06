b68cee50006f8b3dc55cb7fcc9c9746f7f87a08b
Roy Kakkenberg <me@roykakkenberg.nl>
2023-12-06 08:09:40 +0100

    Add day 6 solutions

--- puzzles/day-6/day-6-a.ts
+++ puzzles/day-6/day-6-a.ts
@@ -1,9 +1,47 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+interface Race {
+  time: number;
+  distance: number;
+}
+
+interface RaceOutcomes extends Race {
+  outcomes: number[];
+}
+
 export async function day6a(dataPath?: string) {
-  const data = await readData(dataPath);
-  return 0;
+  const data = (await readData(dataPath)).filter((d) => d !== '');
+
+  const times = data[0].split(': ')[1].split(' ').filter((s) => s !== '').map((s) => parseInt(s));
+  const distances = data[1].split(': ')[1].split(' ').filter((s) => s !== '').map((s) => parseInt(s));
+
+  const races: Race[] = [];
+  times.forEach((time, index) => {
+    races.push({
+      time,
+      distance: distances[index],
+    });
+  });
+
+  const possibleOutcomes: RaceOutcomes[] = races.map((race) => {
+    const outcomes: number[] = new Array(race.time + 1).fill(0)
+      .map((outcome, index) => {
+        const pushTime = index;
+        const speed = pushTime;
+        const remainingTime = race.time - pushTime;
+        return speed * remainingTime;
+      });
+    return {
+      ...race,
+      outcomes,
+    };
+  });
+
+  const winningOutcomes = possibleOutcomes.map((outcome) => {
+    return outcome.outcomes.filter((o) => o > outcome.distance).length;
+  });
+  return winningOutcomes.reduce((total, i) => total * i, 1);
 }
 
 const answer = await day6a();
--- puzzles/day-6/day-6-b.ts
+++ puzzles/day-6/day-6-b.ts
@@ -1,9 +1,54 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+interface Race {
+  time: number;
+  distance: number;
+}
+
+interface RaceOutcomes extends Race {
+  minPushTime: number;
+  maxPushTime: number;
+}
+
 export async function day6b(dataPath?: string) {
-  const data = await readData(dataPath);
-  return 0;
+  const data = (await readData(dataPath)).filter((d) => d !== '');
+
+  const times = [parseInt(data[0].split(': ')[1].split(' ').filter((s) => s !== '')
+    .reduce((total, s) => total + s, ''))];
+  const distances = [parseInt(data[1].split(': ')[1].split(' ').filter((s) => s !== '')
+    .reduce((total, s) => total + s, ''))];
+
+  const races: Race[] = [];
+  times.forEach((time, index) => {
+    races.push({
+      time,
+      distance: distances[index],
+    });
+  });
+
+  const possibleOutcomes: RaceOutcomes[] = races.map((race) => {
+    let minPushTime = 0;
+    let maxPushTime = race.time;
+    for (let i = 0; i <= race.time; i += 1) {
+      if (i * (race.time - i) > race.distance) {
+        minPushTime = i;
+        maxPushTime = race.time - i;
+        break;
+      }
+    }
+
+    return {
+      ...race,
+      minPushTime,
+      maxPushTime,
+    };
+  });
+
+  const winningOutcomes = possibleOutcomes.map((outcome) => {
+    return outcome.maxPushTime - outcome.minPushTime + 1;
+  });
+  return winningOutcomes.reduce((total, i) => total * i, 1);
 }
 
 const answer = await day6b();
