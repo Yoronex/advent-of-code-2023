7f21ea23dd77b61dd5d6fb3ffe76d2cecc9c6957
Yoronex <me@roykakkenberg.nl>
2023-12-08 15:34:06 +0100

    Add day 8 solutions

--- puzzles/day-8/day-8-a.ts
+++ puzzles/day-8/day-8-a.ts
@@ -2,8 +2,27 @@ import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
 export async function day8a(dataPath?: string) {
-  const data = await readData(dataPath);
-  return 0;
+  const data = (await readData(dataPath)).filter((d) => d !== '');
+
+  // Left is first element, right is second element in tuple
+  const instructions = data[0].split('').map((instruction) => instruction === 'R' ? 1 : 0);
+
+  const nodes = new Map<string, string[]>();
+  data.slice(1).forEach((line) => {
+    const [sourceNode, rawTargetNodes] = line.split(' = ');
+    const targetNodes = rawTargetNodes.slice(1, rawTargetNodes.length - 1).split(', ');
+    nodes.set(sourceNode, targetNodes);
+  });
+
+  let steps = 0;
+  let currentNode = 'AAA';
+
+  while (currentNode !== 'ZZZ') {
+    currentNode = nodes.get(currentNode)[instructions[steps % instructions.length]];
+    steps += 1;
+  }
+
+  return steps;
 }
 
 const answer = await day8a();
--- puzzles/day-8/day-8-b.ts
+++ puzzles/day-8/day-8-b.ts
@@ -2,7 +2,44 @@ import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
 export async function day8b(dataPath?: string) {
-  const data = await readData(dataPath);
+  const data = (await readData(dataPath)).filter((d) => d !== '');
+
+  // Left is first element, right is second element in tuple
+  const instructions = data[0].split('').map((instruction) => instruction === 'R' ? 1 : 0);
+
+  const nodes = new Map<string, string[]>();
+  data.slice(1).forEach((line) => {
+    const [sourceNode, rawTargetNodes] = line.split(' = ');
+    const targetNodes = rawTargetNodes.slice(1, rawTargetNodes.length - 1).split(', ');
+    nodes.set(sourceNode, targetNodes);
+  });
+
+  let currentNodes = Array.from(nodes.keys()).filter((node) => node.charAt(2) === 'A');
+
+  // while (currentNodes.some((node) => node.charAt(2) !== 'Z')) {
+  //   currentNodes = currentNodes.map((node) => nodes.get(node)[instructions[steps % instructions.length]]);
+  //   steps += 1;
+  //
+  //   if (steps % 10000 === 0) {
+  //     console.log(steps);
+  //   }
+  // }
+
+  const stepsPerNode = currentNodes.map((node) => {
+    let currentNode = node;
+    let steps = 0;
+
+    while (currentNode.charAt(2) !== 'Z') {
+      currentNode = nodes.get(currentNode)[instructions[steps % instructions.length]];
+      steps += 1;
+    }
+
+    return steps;
+  });
+
+  // TODO: calculate LCM for stepsPerNode
+  // Or just use an online calculator instead, like I did
+
   return 0;
 }
 
