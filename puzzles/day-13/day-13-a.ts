3ba2d1e5d7b6330ca5fd1b9f0cd316e2a746f16b
Roy Kakkenberg <me@roykakkenberg.nl>
2023-12-13 08:52:52 +0100

    Add day 13 solutions

--- puzzles/day-13/day-13-a.ts
+++ puzzles/day-13/day-13-a.ts
@@ -6,41 +6,53 @@ export async function day13a(dataPath?: string) {
 
   let maps: string[][] = [[]];
   data.forEach((line) => {
-    if (line === '') {
+    if (line.trim() === '') {
       maps.push([]);
     } else {
-      maps[maps.length - 1].push(line);
+      maps[maps.length - 1].push(line.trim());
     }
   });
   maps = maps.slice(0, maps.length - 1);
 
-  const width = maps[0][0].length;
-  const height = maps[0].length;
-
-  const horizontalMirrors: (number | undefined)[] = maps.map((map) => {
-    for (let i = 1; i <= height; i += 1) {
+  const verticalMirrors: (number | undefined)[] = maps.map((map) => {
+    const width = map[0].length;
+    const height = map.length;
+    for (let i = 1; i < width; i += 1) {
+      const searchSpace = Math.min(i, width - i);
       // Every line should be equal
       if (map.every((line) => {
         // So every element in a line should be equal
-        const searchSpace = Math.min(i, height - i);
-        for (let x = 1; x <= searchSpace; x += 1) {
-          // Return false if one of the elements is not equal
-          if (line[i - x] !== line[i + x - 1]) return false;
-        }
-        return true;
+        const substring1 = line.substring(i - searchSpace, i).split('').reverse().join('');
+        const substring2 = line.substring(i, i + searchSpace);
+        return substring1 === substring2;
         // Return the index if we found a mirror, undefined otherwise
       })) return i;
     }
     return undefined;
-  });
+  }).filter((i) => i !== undefined);
 
-  const verticalMirrors: (number | undefined)[] = maps.map((map) => {
-    for (let i = 1; i <= width; i += 1) {
-      if
+  const horizontalMirrors: (number | undefined)[] = maps.map((map) => {
+    const width = map[0].length;
+    const height = map.length;
+    for (let i = 1; i < height; i += 1) {
+      const searchSpace = Math.min(i, height - i);
+      let equal = true;
+      for (let x = 1; x <= searchSpace; x += 1) {
+        if (!equal) break;
+        const line1 = map[i - x];
+        const line2 = map[i + x - 1];
+        if (line1 !== line2) {
+          equal = false;
+          break;
+        }
+      }
+      if (equal) return i;
     }
-  })
+    return undefined;
+  }).filter((i) => i !== undefined);
 
-  return 0;
+  return verticalMirrors.reduce((total, i) => total + i, 0)
+    + 100 * horizontalMirrors.reduce((total, i) => total + i, 0);
 }
 
 const answer = await day13a();
--- puzzles/day-13/day-13-b.ts
+++ puzzles/day-13/day-13-b.ts
@@ -1,9 +1,123 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+const differenceIndices = (line1: string, line2: string) => {
+  const l1 = line1.split('');
+  const l2 = line2.split('');
+  const indices = l1.map((l, i) => i);
+
+  return indices.filter((i) => l1[i] !== l2[i]);
+}
+
 export async function day13b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  let maps: string[][] = [[]];
+  data.forEach((line) => {
+    if (line.trim() === '') {
+      maps.push([]);
+    } else {
+      maps[maps.length - 1].push(line.trim());
+    }
+  });
+  maps = maps.slice(0, maps.length - 1);
+
+  // Original mirrors
+  const verticalMirrors: number[][] = maps.map((map) => {
+    const result: number[] = [];
+    const width = map[0].length;
+    const height = map.length;
+    for (let i = 1; i < width; i += 1) {
+      const searchSpace = Math.min(i, width - i);
+      // Every line should be equal
+      if (map.every((line) => {
+        // So every element in a line should be equal
+        const substring1 = line.substring(i - searchSpace, i).split('').reverse().join('');
+        const substring2 = line.substring(i, i + searchSpace);
+        return substring1 === substring2;
+        // Return the index if we found a mirror, undefined otherwise
+      })) result.push(i);
+    }
+    return result;
+  });
+
+  const horizontalMirrors: number[][] = maps.map((map) => {
+    const result: number[] = [];
+    const width = map[0].length;
+    const height = map.length;
+    for (let i = 1; i < height; i += 1) {
+      const searchSpace = Math.min(i, height - i);
+      let equal = true;
+      for (let x = 1; x <= searchSpace; x += 1) {
+        if (!equal) break;
+        const line1 = map[i - x];
+        const line2 = map[i + x - 1];
+        if (line1 !== line2) {
+          equal = false;
+          break;
+        }
+      }
+      if (equal) result.push(i);
+    }
+    return result;
+  });
+
+  const verticalMirrorsWithSmudge: number[][] = maps.map((map) => {
+    const result: number[] = [];
+    const width = map[0].length;
+    const height = map.length;
+    for (let i = 1; i < width; i += 1) {
+      const searchSpace = Math.min(i, width - i);
+      let smudge = false;
+      // Every line should be equal
+      if (map.every((line) => {
+        // So every element in a line should be equal
+        const substring1 = line.substring(i - searchSpace, i).split('').reverse().join('');
+        const substring2 = line.substring(i, i + searchSpace);
+        const hasSmudge = differenceIndices(substring1, substring2).length === 1;
+        if (substring1 === substring2 || (!smudge && hasSmudge)) {
+          if (hasSmudge) smudge = true;
+          return true;
+        }
+        // Return the index if we found a mirror, undefined otherwise
+      })) result.push(i);
+    }
+    return result;
+  });
+
+  const horizontalMirrorsWithSmudge: number[][] = maps.map((map) => {
+    const result: number[] = [];
+    const width = map[0].length;
+    const height = map.length;
+    for (let i = 1; i < height; i += 1) {
+      const searchSpace = Math.min(i, height - i);
+      let equal = true;
+      let smudge = false;
+      for (let x = 1; x <= searchSpace; x += 1) {
+        if (!equal) break;
+        const line1 = map[i - x];
+        const line2 = map[i + x - 1];
+        const hasSmudge = differenceIndices(line1, line2).length === 1;
+        if (!(line1 === line2 || (!smudge && hasSmudge))) {
+          if (hasSmudge) smudge = true;
+          equal = false;
+          break;
+        }
+      }
+      if (equal) result.push(i);
+    }
+    return result
+  });
+
+  const vertMir = verticalMirrorsWithSmudge.map((mirrors, i) => {
+    return mirrors.filter((mir) => !verticalMirrors[i].includes(mir));
+  }).flat();
+  const horMir = horizontalMirrorsWithSmudge.map((mirrors, i) => {
+    return mirrors.filter((mir) => !horizontalMirrors[i].includes(mir));
+  }).flat();
+
+  return vertMir.reduce((total, i) => total + i, 0)
+    + 100 * horMir.reduce((total, i) => total + i, 0);
 }
 
 const answer = await day13b();
